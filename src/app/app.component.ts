f8ac98510ef43ee92f00446be665576ead73be4d
yogesan66 <105202046+yogesan66@users.noreply.github.com>
2023-01-16 06:04:52 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+
+  regular_tasks :Array<any> = [
+    {
+      task_name:"running",
+      done:false
+    },
+    {
+      task_name:"brushing",
+      done:false
+    },
+    {
+      task_name:"eating",
+      done:false
+    },
+    {
+      task_name:"playing",
+      done:false
+    },
+    {
+      task_name:"sleeping",
+      done:false
+    }
+  ]
+
+  outstanding_tasks :Array<any> = ["running","brushing","eating","playing","sleeping"]
+this: any;
+  
+done :Boolean = false;
+
+  on(i: any){
+    // this.done = true;
+    // if(this.done == true){
+    // i = 0
+    this.outstanding_tasks.splice(i,1);
+    console.log(i)
+    // this.done = false
+    // }
+  }
+}
